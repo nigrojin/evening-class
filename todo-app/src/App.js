2e8c76d358d1e6ac8067395174734190229dc9d8
sonsungjong <tbxmtbfm@naver.com>
2024-03-20 21:43:00 +0900

    3/20

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -18,4 +18,139 @@ if (!localStorage.getItem("tasksData")) {
   saveData(SEED);
 }
 
-export default function App() {}
+// 필터 맵 객체
+const FILTER_MAP = {
+  전체: () => true,
+  완료: (task) => task.completed,
+  미완료: (task) => !task.completed
+}
+
+// 필터 이름 - 전체, 완료, 미완료
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
+export default function App() {
+  const initialTasks = JSON.parse(localStorage.getItem("tasksData"));
+  const [tasks, setTasks] = useState(initialTasks);
+  // 현재 적용중인 필터
+  const [filter, setFilter] = useState("전체");
+
+  // 키 스테이트 추적
+  console.log(tasks);
+
+  // 로컬 스토리지 동기화
+  useEffect(() => {
+    saveData(tasks);
+  }, [tasks])
+
+  // 문서 타이틀 업데이트
+  useEffect(() => {
+    document.title = "Todo App"
+  }, [])
+
+  // 할일 추가 처리
+  function addTask() {
+    // 새 할일 객체 생성
+    const newTask = {
+      id: "todo-" + Date.now(),
+      name: "",
+      completed: false
+    }
+
+    // tasks 업데이트
+    const updatedTasks = [newTask, ...tasks];
+
+    setTasks(updatedTasks);
+  }
+
+  // 할일 삭제 처리
+  function deleteTask(id) {
+    // 전달받은 id와 일치하는 id를 가진 task를 제외한다
+    const remainingTasks = tasks.filter(task => task.id !== id);
+
+    setTasks(remainingTasks);
+  }
+
+  // 할일 수정 처리
+  function editTask(id, newName) {
+    // tasks 업데이트
+    const editedTasks = tasks.map(task => {
+      // 전달받은 id와 일치하는 id를 가진 task의 name을 newName으로 수정한다
+      if (task.id == id) {
+        return { ...task, name: newName }
+      }
+      // 나머지 task는 그대로 리턴한다
+      return task;
+    })
+
+    setTasks(editedTasks);
+  }
+
+  // 할일 상태변경 처리
+  function toggleTaskCompleted(id) {
+    const updatedTasks = tasks.map(task => {
+      // 전달받은 id와 일치하는 id를 가진 task의 completed를 현재 상태의 반대로 업데이트한다
+      if (task.id == id) {
+        return { ...task, completed: !task.completed }
+      }
+      // 나머지는 그대로 리턴한다
+      return task;
+    })
+
+    setTasks(updatedTasks);
+  }
+
+  // 필터 버튼 렌더링
+  const filterButtons = FILTER_NAMES.map(name => (
+    <button 
+      key={name}
+      className="disabled:font-bold"
+      // 필터 버튼의 이름과 현재 필터(filter)가 일치하는 경우 비활성화
+      disabled={name == filter}
+      onClick={() => setFilter(name)}
+    >
+      {name}
+    </button>
+  ))
+
+  // 할일 목록 렌더링 - 필터링한 결과를 순회한다
+  const taskList = tasks.filter(FILTER_MAP[filter]).map(task => (
+    // 컴포넌트 리스트 렌더링
+    <Todo 
+      key={task.id}
+      id={task.id}
+      name={task.name}
+      completed={task.completed}
+      deleteTask={deleteTask}
+      toggleTaskCompleted={toggleTaskCompleted}
+      editTask={editTask}
+    />
+  ))
+
+  return (
+    <div className="max-w-sm mx-auto px-4">
+      <h1 className="text-2xl font-semibold text-center my-8">
+        Todo App
+      </h1>
+
+      <div className="flex justify-between mb-4">
+        <div className="flex gap-2 py-1">
+          {filterButtons}
+        </div>
+        <button
+          type="submit"
+          className="px-2 py-1 rounded bg-blue-500 hover:bg-blue-600 text-white font-semibold"
+          onClick={addTask}
+        >
+          새 할일 +
+        </button>
+      </div>
+
+      <h2 className="mb-4 text-gray-500">
+        총 {taskList.length}개 있습니다
+      </h2>
+      <ul>
+        {taskList}
+      </ul>
+    </div>
+  )
+}
--- todo-app/src/components/Todo.js
+++ todo-app/src/components/Todo.js
@@ -1,1 +1,58 @@
-export default function Todo() {};
+export default function Todo({
+  id,
+  name,
+  completed,
+  deleteTask,
+  toggleTaskCompleted,
+  editTask
+}) {
+
+  // 삭제 처리
+  function handleDelete() {
+    // 사용자가 '예' 하는 경우 true, '아니오' 하는 경우 false를 리턴한다
+    var q = window.confirm("삭제하시겠습니까?");
+
+    if (q) {
+      deleteTask(id);
+    }
+  };
+
+  return (
+    <div className="flex h-16 items-center mb-3 border rounded">
+      {/* 체크박스 */}
+      <label className="px-4">
+        <input
+          type="checkbox"
+          id={id}
+          className="peer hidden"
+          checked={completed}
+          // 체크박스의 change이벤트 - 체크 상태가 바뀔 때 발생한다
+          onChange={() => toggleTaskCompleted(id)}
+        />
+        <svg
+          className="w-6 fill-gray-200 peer-checked:fill-blue-400"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 512 512"
+        >
+          <path d="M256 48a208 208 0 1 1 0 416 208 208 0 1 1 0-416zm0 464A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM369 209c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-111 111-47-47c-9.4-9.4-24.6-9.4-33.9 0s-9.4 24.6 0 33.9l64 64c9.4 9.4 24.6 9.4 33.9 0L369 209z" />
+        </svg>
+      </label>
+
+      {/* 이름 (입력란) */}
+      <input
+        type="text"
+        className="grow outline-none focus:border-b focus:border-black"
+        value={name}
+        onChange={(e) => editTask(id, e.target.value)}
+      />
+
+      {/* 삭제 버튼 */}
+      <button
+        className="px-4 text-gray-400"
+        onClick={handleDelete}
+      >
+        -
+      </button>
+    </div>
+  )
+};
