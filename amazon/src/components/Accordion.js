317ec3b27a6f7647570775fd2072344c8a03e196
sonsungjong <tbxmtbfm@naver.com>
2024-04-04 21:46:19 +0900

    4/4

---
+++ amazon/src/components/Accordion.js
@@ -0,0 +1,4 @@
+export default function Accordion() {}
+
+// 아코디언의 각각의 탭
+function Tab() {}
---
+++ lab/main.js
@@ -0,0 +1,25 @@
+
+
+// class Node {
+//   constructor (val) {
+//     this.key = val;
+//     this.left = null;
+//     this.right = null;
+//   }
+// }
+
+
+// var root = null;
+
+// root = new Node(1);
+
+// // insert 
+// root.left = new Node(2);
+// root.right = new Node(3);
+// root.left.left = new Node(4);
+
+// console.log(root);
+
+var arr = [10, 20, 30, 40, 50, 60, 70, 80, 90];
+
+console.log(arr.indexOf(20));
